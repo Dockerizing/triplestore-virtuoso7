9ec5d6189dd3e3756328226e796b1e1e23e3088a
Markus Ackermann <neradis@fusionfactory.de>
2015-06-09 17:22:27 +0200

    Modified /run.sh to ensure graceful stop of container. The start script reacts to SIGTERM send by docker now and shuts down the Virtuoso cleanly. Before it was reaped with a SIGKILL from the docker daemon after a timeout, dragging VOS into the grave without proper shutdown.

--- run.sh
+++ run.sh
@@ -1,10 +1,24 @@
-#!/bin/sh
-
+#!/bin/bash
 VOSCMD=/usr/bin/virtuoso-t
 CONFIG=virtuoso.ini
 
+# allowing for clean shutdown of background jobs
+cleanup () {
+  echo "stopping virtuoso..."
+  [[ -n $vospid ]] && kill -TERM "$vospid"
+
+  exit 0
+}
+
+trap 'cleanup' INT TERM
+
 # Change password
+echo "setting virtuoso dba password..."
 ${VOSCMD} +configfile ${CONFIG} +foreground +pwdold dba +pwddba ${PWDDBA}
 
 # Start server
-${VOSCMD} +configfile ${CONFIG} +foreground
+echo "starting virtuoso..."
+${VOSCMD} +configfile ${CONFIG} +foreground &
+vospid=$!
+
+wait $vospid
